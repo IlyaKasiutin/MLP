7fd1e253dbf42b756f5fef607c6be79f5d5bf520
IlyaKasiutin <boxfighterilya@gmail.com>
2023-06-09 07:24:18 +0300

    Замер времени

--- main.c
+++ main.c
@@ -12,11 +12,14 @@ int main() {
 	srand(time(NULL));
 
 	//TRAINING
-	// int number_imgs = 10000;
+	// int number_imgs = 1000;
 	// Img** imgs = csv_to_imgs("./data/mnist_train.csv", number_imgs);
 	// //img_print(imgs[3]);
 	// NeuralNetwork* net = network_create(784, 300, 10, 0.1);
+	// time_t start = time(NULL);
 	// network_train_batch_imgs(net, imgs, number_imgs);
+	// time_t end = time(NULL);
+	// printf("Training time: %.1f seconds\n", difftime(end, start));
 	// network_save(net, "testing_net");
 
 	// PREDICTION
